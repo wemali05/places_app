aa8ccf46aad34cafc67f83f5ff5173d581b51a75
weali05 <alfredwemali05@gmail.com>
2025-01-02 12:53:22 +0300

    place details screen

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/place_details.dart';
 import 'package:flutter/material.dart';
 
 class PlacesList extends StatelessWidget {
@@ -23,10 +24,17 @@ class PlacesList extends StatelessWidget {
       itemBuilder: (ctx, index) => ListTile(
         title: Text(
           places[index].title,
-          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                 color: Theme.of(context).colorScheme.onBackground,
               ),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailsScreen(place: places[index]),
+            ),
+          );
+        },
       ),
     );
   }
