38599bffa571a417416dcae80b09251a303e541d
weali05 <alfredwemali05@gmail.com>
2025-01-02 14:42:36 +0300

    preview picked image

--- lib/screens/place_details.dart
+++ lib/screens/place_details.dart
@@ -13,11 +13,15 @@ class PlaceDetailsScreen extends StatelessWidget {
         title: Text(place.title),
       ),
       body: Center(
-        child: Text(
-          place.title,
-          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
+        child: Stack(
+          children: [
+            Image.file(
+              place.image,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ],
         ),
       ),
     );
